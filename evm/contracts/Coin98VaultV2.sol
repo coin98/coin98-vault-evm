c852b313cef878f0581492e9ab8f35f17b402f26
Tung Nguyen <87560224+tungnguyennhat98@users.noreply.github.com>
2022-05-06 15:28:20 +0700

    factory with proxy support

--- evm/contracts/Coin98VaultV2.sol
+++ evm/contracts/Coin98VaultV2.sol
@@ -465,10 +465,262 @@ library MerkleProof {
   }
 }
 
+abstract contract Initializable {
+  /**
+  * @dev Indicates that the contract has been initialized.
+  * @custom:oz-retyped-from bool
+  */
+  uint8 private _initialized;
+
+  /**
+  * @dev Indicates that the contract is in the process of being initialized.
+  */
+  bool private _initializing;
+
+  /**
+  * @dev A modifier that defines a protected initializer function that can be invoked at most once. In its scope,
+  * `onlyInitializing` functions can be used to initialize parent contracts. Equivalent to `reinitializer(1)`.
+  */
+  modifier initializer() {
+    bool isTopLevelCall = _setInitializedVersion(1);
+    if (isTopLevelCall) {
+        _initializing = true;
+    }
+    _;
+    if (isTopLevelCall) {
+        _initializing = false;
+    }
+  }
+
+  /**
+  * @dev A modifier that defines a protected reinitializer function that can be invoked at most once, and only if the
+  * contract hasn't been initialized to a greater version before. In its scope, `onlyInitializing` functions can be
+  * used to initialize parent contracts.
+  *
+  * `initializer` is equivalent to `reinitializer(1)`, so a reinitializer may be used after the original
+  * initialization step. This is essential to configure modules that are added through upgrades and that require
+  * initialization.
+  *
+  * Note that versions can jump in increments greater than 1; this implies that if multiple reinitializers coexist in
+  * a contract, executing them in the right order is up to the developer or operator.
+  */
+  modifier reinitializer(uint8 version) {
+    bool isTopLevelCall = _setInitializedVersion(version);
+    if (isTopLevelCall) {
+        _initializing = true;
+    }
+    _;
+    if (isTopLevelCall) {
+        _initializing = false;
+    }
+  }
+
+  /**
+  * @dev Modifier to protect an initialization function so that it can only be invoked by functions with the
+  * {initializer} and {reinitializer} modifiers, directly or indirectly.
+  */
+  modifier onlyInitializing() {
+    require(_initializing, "Initializable: contract is not initializing");
+    _;
+  }
+
+  /**
+  * @dev Locks the contract, preventing any future reinitialization. This cannot be part of an initializer call.
+  * Calling this in the constructor of a contract will prevent that contract from being initialized or reinitialized
+  * to any version. It is recommended to use this to lock implementation contracts that are designed to be called
+  * through proxies.
+  */
+  function _disableInitializers() internal virtual {
+    _setInitializedVersion(type(uint8).max);
+  }
+
+  function _setInitializedVersion(uint8 version) private returns (bool) {
+    // If the contract is initializing we ignore whether _initialized is set in order to support multiple
+    // inheritance patterns, but we only do this in the context of a constructor, and for the lowest level
+    // of initializers, because in other contexts the contract may have been reentered.
+    if (_initializing) {
+        require(
+            version == 1 && !Address.isContract(address(this)),
+            "Initializable: contract is already initialized"
+        );
+        return false;
+    } else {
+        require(_initialized < version, "Initializable: contract is already initialized");
+        _initialized = version;
+        return true;
+    }
+  }
+}
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract OwnableUpgradeable is Initializable, Context {
+  address private _owner;
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+  /**
+  * @dev Initializes the contract setting the deployer as the initial owner.
+  */
+  function __Ownable_init() internal onlyInitializing {
+    __Ownable_init_unchained();
+  }
+
+  function __Ownable_init_unchained() internal onlyInitializing {
+    _transferOwnership(_msgSender());
+  }
+
+  /**
+  * @dev Returns the address of the current owner.
+  */
+  function owner() public view virtual returns (address) {
+    return _owner;
+  }
+
+  /**
+  * @dev Throws if called by any account other than the owner.
+  */
+  modifier onlyOwner() {
+    require(owner() == _msgSender(), "Ownable: caller is not the owner");
+    _;
+  }
+
+  /**
+  * @dev Leaves the contract without owner. It will not be possible to call
+  * `onlyOwner` functions anymore. Can only be called by the current owner.
+  *
+  * NOTE: Renouncing ownership will leave the contract without an owner,
+  * thereby removing any functionality that is only available to the owner.
+  */
+  function renounceOwnership() public virtual onlyOwner {
+    _transferOwnership(address(0));
+  }
+
+  /**
+  * @dev Transfers ownership of the contract to a new account (`newOwner`).
+  * Can only be called by the current owner.
+  */
+  function transferOwnership(address newOwner) public virtual onlyOwner {
+    require(newOwner != address(0), "Ownable: new owner is the zero address");
+    _transferOwnership(newOwner);
+  }
+
+  /**
+  * @dev Transfers ownership of the contract to a new account (`newOwner`).
+  * Internal function without access restriction.
+  */
+  function _transferOwnership(address newOwner) internal virtual {
+    address oldOwner = _owner;
+    _owner = newOwner;
+    emit OwnershipTransferred(oldOwner, newOwner);
+  }
+
+  /**
+  * @dev This empty reserved space is put in place to allow future versions to add new
+  * variables without shifting down storage in the inheritance chain.
+  * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+  */
+  uint256[49] private __gap;
+}
+
+/**
+ * @dev https://eips.ethereum.org/EIPS/eip-1167[EIP 1167] is a standard for
+ * deploying minimal proxy contracts, also known as "clones".
+ *
+ * > To simply and cheaply clone contract functionality in an immutable way, this standard specifies
+ * > a minimal bytecode implementation that delegates all calls to a known, fixed address.
+ *
+ * The library includes functions to deploy a proxy using either `create` (traditional deployment) or `create2`
+ * (salted deterministic deployment). It also includes functions to predict the addresses of clones deployed using the
+ * deterministic method.
+ *
+ * _Available since v3.4._
+ */
+library Clones {
+  /**
+  * @dev Deploys and returns the address of a clone that mimics the behaviour of `implementation`.
+  *
+  * This function uses the create opcode, which should never revert.
+  */
+  function clone(address implementation) internal returns (address instance) {
+    assembly {
+      let ptr := mload(0x40)
+      mstore(ptr, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
+      mstore(add(ptr, 0x14), shl(0x60, implementation))
+      mstore(add(ptr, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+      instance := create(0, ptr, 0x37)
+    }
+    require(instance != address(0), "ERC1167: create failed");
+  }
+
+  /**
+  * @dev Deploys and returns the address of a clone that mimics the behaviour of `implementation`.
+  *
+  * This function uses the create2 opcode and a `salt` to deterministically deploy
+  * the clone. Using the same `implementation` and `salt` multiple time will revert, since
+  * the clones cannot be deployed twice at the same address.
+  */
+  function cloneDeterministic(address implementation, bytes32 salt) internal returns (address instance) {
+    assembly {
+      let ptr := mload(0x40)
+      mstore(ptr, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
+      mstore(add(ptr, 0x14), shl(0x60, implementation))
+      mstore(add(ptr, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+      instance := create2(0, ptr, 0x37, salt)
+    }
+    require(instance != address(0), "ERC1167: create2 failed");
+  }
+
+  /**
+  * @dev Computes the address of a clone deployed using {Clones-cloneDeterministic}.
+  */
+  function predictDeterministicAddress(
+    address implementation,
+    bytes32 salt,
+    address deployer
+  ) internal pure returns (address predicted) {
+    assembly {
+      let ptr := mload(0x40)
+      mstore(ptr, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
+      mstore(add(ptr, 0x14), shl(0x60, implementation))
+      mstore(add(ptr, 0x28), 0x5af43d82803e903d91602b57fd5bf3ff00000000000000000000000000000000)
+      mstore(add(ptr, 0x38), shl(0x60, deployer))
+      mstore(add(ptr, 0x4c), salt)
+      mstore(add(ptr, 0x6c), keccak256(ptr, 0x37))
+      predicted := keccak256(add(ptr, 0x37), 0x55)
+    }
+  }
+
+  /**
+  * @dev Computes the address of a clone deployed using {Clones-cloneDeterministic}.
+  */
+  function predictDeterministicAddress(address implementation, bytes32 salt)
+    internal
+    view
+    returns (address predicted)
+  {
+    return predictDeterministicAddress(implementation, salt, address(this));
+  }
+}
+
+interface ICoin98Vault {
+  function init() external virtual;
+}
+
 /**
  * @dev Coin98Vault contract to enable vesting funds to investors
  */
-contract Coin98Vault is Ownable, Payable {
+contract Coin98Vault is ICoin98Vault, OwnableUpgradeable, Payable {
 
   using SafeERC20 for IERC20;
 
@@ -477,13 +729,6 @@ contract Coin98Vault is Ownable, Payable {
   mapping(address => bool) private _adminStatuses;
   mapping(uint256 => EventData) private _eventDatas;
   mapping(uint256 => mapping(uint256 => bool)) private _eventRedemptions;
-
-  /// @dev Initialize a new vault
-  /// @param factory_ Back reference to the factory initialized this vault for global configuration
-  /// @param owner_ Owner of this vault
-  constructor(address factory_, address owner_) Ownable(owner_) {
-    _factory = factory_;
-  }
 
   struct EventData {
     uint256 timestamp;
@@ -531,6 +776,12 @@ contract Coin98Vault is Ownable, Payable {
   /// @param index_ index of redemption pre-assigned to user
   function isRedeemed(uint256 eventId_, uint256 index_) public view returns (bool) {
     return _eventRedemptions[eventId_][index_];
+  }
+
+  /// @dev Initial vault
+  function init() external override initializer {
+    __Ownable_init();
+    _factory = msg.sender;
   }
 
   /// @dev claim the token which user is eligible from schedule
@@ -690,9 +941,11 @@ contract Coin98VaultFactory is Ownable, Payable, IVaultConfig {
   uint256 private _fee;
   uint256 private _gasLimit;
   uint256 private _ownerReward;
+  address private _implementation;
   address[] private _vaults;
 
-  constructor () Ownable(_msgSender()) {
+  constructor (address _vaultImplementation) Ownable(_msgSender()) {
+    _implementation = _vaultImplementation;
     _gasLimit = 9000;
   }
 
@@ -725,12 +978,32 @@ contract Coin98VaultFactory is Ownable, Payable, IVaultConfig {
     return _vaults;
   }
 
+  /// @dev Get implementation
+  function getImplementation() public view returns(address) {
+    return _implementation;
+  }
+
+  /// @dev Set new implementation
+  /// @param _newImplementation New implementation of vault
+  function setImplementation(address _newImplementation) public onlyOwner {
+    _implementation = _newImplementation;
+  }
+
   /// @dev create a new vault
   /// @param owner_ Owner of newly created vault
-  function createVault(address owner_) external returns (Coin98Vault vault) {
-    vault = new Coin98Vault(address(this), owner_);
+  /// @param salt_ an arbitrary value
+  function createVault(address owner_, bytes32 salt_) external returns (address vault) {
+    address vault = Clones.cloneDeterministic(_implementation, salt_);
+
+    ICoin98Vault(vault).init();
+    Ownable(vault).transferOwnership(owner_);
+
     _vaults.push(address(vault));
     emit Created(address(vault));
+  }
+
+  function getVaultAddress(bytes32 salt_) public view returns(address) {
+    return Clones.predictDeterministicAddress(_implementation, salt_);
   }
 
   function setGasLimit(uint256 limit_) public onlyOwner {
