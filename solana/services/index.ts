03c3fa3393ea881ba8c3b0d1f3833bedafa2fc14
Bui Duc Huy <duchuy.124dk@gmail.com>
2022-06-01 15:44:35 +0700

    fix(package): index file

---
+++ solana/services/index.ts
@@ -0,0 +1,3 @@
+export { VaultService } from "./vault.service"
+export { VaultInstructionService } from "./vault_instruction.service"
+export { MerkleDistributionService } from "./merkle_distributor.service"
