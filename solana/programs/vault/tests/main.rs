ffc2383a1bcd00f836c92d39fd72c5bc49c1d04b
Leon Nguyen <vicnguyenvuong@gmail.com>
2022-08-09 13:37:59 +0700

    fix error when create schedule

--- solana/programs/vault/tests/main.rs
+++ solana/programs/vault/tests/main.rs
@@ -40,8 +40,8 @@ async fn test_create_vault() {
     create_mint(&mut context, &cusd_mint, &payer_wallet.pubkey(), None).await.unwrap();
 
 
-    let payer_c98_token_account = create_associated_token_account(&mut context, &payer_wallet.pubkey(), &c98_mint.pubkey()).await.unwrap();
-    let payer_cusd_token_account = create_associated_token_account(&mut context, &payer_wallet.pubkey(), &cusd_mint.pubkey()).await.unwrap();
+    // let payer_c98_token_account = create_associated_token_account(&mut context, &payer_wallet.pubkey(), &c98_mint.pubkey()).await.unwrap();
+    // let payer_cusd_token_account = create_associated_token_account(&mut context, &payer_wallet.pubkey(), &cusd_mint.pubkey()).await.unwrap();
 
     let user1_c98_token_account = create_associated_token_account(&mut context, &user1.pubkey(), &c98_mint.pubkey()).await.unwrap();
     let user1_cusd_token_account = create_associated_token_account(&mut context, &user1.pubkey(), &cusd_mint.pubkey()).await.unwrap();
@@ -53,11 +53,16 @@ async fn test_create_vault() {
     let create_vault_data = create_vault_data_instruction(&payer_wallet.pubkey(), feed_path.clone(), signer_nonce);
     let set_vault_data = set_vault_data_instruction(&payer_wallet.pubkey(), feed_path, Vec::from([authority.pubkey()]));       
 
+    let vault_c98_token_account = create_associated_token_account(&mut context, &vault_signer_address, &c98_mint.pubkey()).await.unwrap();
+    let vault_cusd_token_account = create_associated_token_account(&mut context, &vault_signer_address, &cusd_mint.pubkey()).await.unwrap();
+
     process_transaction(&mut context, &Vec::from([create_vault_data, set_vault_data]), &Vec::from([&payer_wallet])).await.unwrap();
 
-    mint_tokens(&mut context, &c98_mint.pubkey(), &payer_c98_token_account, 1_000_000_000_000, &payer_wallet.pubkey(), Some(&payer_wallet)).await.unwrap();
+    // mint_tokens(&mut context, &c98_mint.pubkey(), &payer_c98_token_account, 1_000_000_000_000, &payer_wallet.pubkey(), Some(&payer_wallet)).await.unwrap();
     mint_tokens(&mut context, &c98_mint.pubkey(), &user1_c98_token_account, 1_000_000_000_000, &payer_wallet.pubkey(), Some(&payer_wallet)).await.unwrap();
     mint_tokens(&mut context, &cusd_mint.pubkey(), &user1_cusd_token_account, 1_000_000_000_000, &payer_wallet.pubkey(), Some(&payer_wallet)).await.unwrap();
+    mint_tokens(&mut context, &c98_mint.pubkey(), &vault_c98_token_account, 1_000_000_000_000, &payer_wallet.pubkey(), Some(&payer_wallet)).await.unwrap();
+    mint_tokens(&mut context, &cusd_mint.pubkey(), &vault_cusd_token_account, 1_000_000_000_000, &payer_wallet.pubkey(), Some(&payer_wallet)).await.unwrap();
     
 
 
@@ -65,14 +70,14 @@ async fn test_create_vault() {
     index: 0,
     address: user1.pubkey(),
     receiving_amount: 1000000,
-    sending_amount: 0,
+    sending_amount: 1000,
     };  
 
     let user2_data = vault::RedemptionParams{
     index: 1,
     address: user2.pubkey(),
     receiving_amount: 1000000,
-    sending_amount: 0,
+    sending_amount: 1000,
     };  
     let hash_user1 = hash_redemption(user1_data);
     let hash_user2 = hash_redemption(user2_data);
@@ -81,7 +86,15 @@ async fn test_create_vault() {
     println!("{:?}", merkle_root);
     // create schedule
     let (_schedule_address,_) = find_schedule_address(1);
-    let create_schedule_data = create_schedule_data_instruction(&payer_wallet.pubkey(), &vault_address, 2, 1, 0, merkle_root ,&c98_mint.pubkey(), &payer_c98_token_account, &cusd_mint.pubkey(), &payer_cusd_token_account);
+    let create_schedule_data = create_schedule_data_instruction(
+        &payer_wallet.pubkey(), 
+        &vault_address, 
+        2, 1, 0, 
+        merkle_root ,
+        &c98_mint.pubkey(), 
+        &vault_c98_token_account, 
+        &cusd_mint.pubkey(), 
+        &vault_cusd_token_account);
     let set_schedule_status_data = set_schedule_status_data_instruction(&payer_wallet.pubkey(), &vault_address, 1, true);
     process_transaction(&mut context, &Vec::from([create_schedule_data, set_schedule_status_data]), &Vec::from([&payer_wallet])).await.unwrap();
 
@@ -90,17 +103,19 @@ async fn test_create_vault() {
     println!("verify");
     let value : bool = vault::shared::verify_proof(user1_proofs.clone(), merkle_root, hash_user1);
     println!("{:?}", value);
-    let redeem_token_data = redeem_token_data_instruction(
+    let redeem_token_data = redeem_token_with_fee_data_instruction(
         &vault_address, 
         &_schedule_address, 
         &vault_signer_address, 
-        &payer_c98_token_account, 
+        &vault_c98_token_account, 
+        &vault_cusd_token_account,
         &user1.pubkey(), 
         &user1_c98_token_account, 
+        &user1_cusd_token_account,
         0, 
         user1_proofs,
         1000000, 
-        0);
+        1000);
     process_transaction(&mut context, &Vec::from([redeem_token_data]), &Vec::from([&user1])).await.unwrap();
 }
 
