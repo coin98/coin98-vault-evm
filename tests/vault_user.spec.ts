8975c8b6267fe0e5db64178b240f0085d6f75ec8
Tung Nguyen <87560224+tungng98@users.noreply.github.com>
2023-06-01 14:01:16 +0700

    more user tests

--- tests/vault_user.spec.ts
+++ tests/vault_user.spec.ts
@@ -18,6 +18,7 @@ import {
   WhitelistData,
   createWhitelistTree
 } from './common';
+import { expect } from 'chai';
 
 describe('vault administrative task tests', function() {
   let owner: Signer;
@@ -80,7 +81,10 @@ describe('vault administrative task tests', function() {
     const proofs = whilelistProofs.map(node => '0x' + node.hash.toString('hex'));
 
     await hheh.time.increaseTo(currentTimestamp);
+    const balanceBefore = await c98Token.balanceOf(account1Address);
     await sut.connect(account1).redeem(salt, 0, currentTimestamp, account1Address, 1000000, 0, proofs);
+    const balanceAfter = await c98Token.balanceOf(account1Address);
+    expect((balanceAfter.sub(balanceBefore)).toNumber()).eq(1000000);
   });
 
   it('redeem legacy token successful', async function() {
@@ -100,6 +104,161 @@ describe('vault administrative task tests', function() {
     const proofs = whilelistProofs.map(node => '0x' + node.hash.toString('hex'));
 
     await hheh.time.increaseTo(currentTimestamp);
+    const balanceBefore = await usdtToken.balanceOf(account2Address);
     await sut.connect(account2).redeem(salt, 1, currentTimestamp, account2Address, 1000000, 0, proofs);
+    const balanceAfter = await usdtToken.balanceOf(account2Address);
+    expect((balanceAfter.sub(balanceBefore)).toNumber()).eq(1000000);
+  });
+
+  it('other user claim', async function() {
+    let currentTimestamp = new Date().getTime();
+    const salt = utils.solidityKeccak256(['uint256', 'uint256', 'uint256'], [currentTimestamp, Math.floor(Math.random() * 1000000000), 1003]);
+    let whitelists = [
+      <WhitelistData>{ index: 0, unlockTimestamp: currentTimestamp, recipientAddress: account1Address, receivingAmount: 1000000, sendingAmount: 0 },
+      <WhitelistData>{ index: 1, unlockTimestamp: currentTimestamp, recipientAddress: account2Address, receivingAmount: 1000000, sendingAmount: 0 },
+      <WhitelistData>{ index: 2, unlockTimestamp: currentTimestamp, recipientAddress: account3Address, receivingAmount: 1000000, sendingAmount: 0 },
+    ];
+    const whitelistTree = createWhitelistTree(whitelists);
+    const whitelistRoot = '0x' + whitelistTree.root().hash.toString('hex');
+    await sut.connect(owner).createEvent(salt, whitelistRoot, c98Token.address, ZERO_ADDRESS);
+
+    await c98Token.connect(owner).mint(sut.address, 3000000);
+    const whilelistProofs = whitelistTree.proofs(0);
+    const proofs = whilelistProofs.map(node => '0x' + node.hash.toString('hex'));
+
+    await hheh.time.increaseTo(currentTimestamp);
+    const balanceBefore = await c98Token.balanceOf(account1Address);
+    const balanceBefore2 = await c98Token.balanceOf(account2Address);
+    await sut.connect(account2).redeem(salt, 0, currentTimestamp, account1Address, 1000000, 0, proofs);
+    const balanceAfter = await c98Token.balanceOf(account1Address);
+    const balanceAfter2 = await c98Token.balanceOf(account2Address);
+    expect((balanceAfter.sub(balanceBefore)).toNumber()).eq(1000000);
+    expect((balanceAfter2.sub(balanceBefore2)).toNumber()).eq(0);
+  });
+
+  it('wrong index', async function() {
+    let currentTimestamp = new Date().getTime();
+    const salt = utils.solidityKeccak256(['uint256', 'uint256', 'uint256'], [currentTimestamp, Math.floor(Math.random() * 1000000000), 1004]);
+    let whitelists = [
+      <WhitelistData>{ index: 0, unlockTimestamp: currentTimestamp, recipientAddress: account1Address, receivingAmount: 1000000, sendingAmount: 0 },
+      <WhitelistData>{ index: 1, unlockTimestamp: currentTimestamp, recipientAddress: account2Address, receivingAmount: 1000000, sendingAmount: 0 },
+      <WhitelistData>{ index: 2, unlockTimestamp: currentTimestamp, recipientAddress: account3Address, receivingAmount: 1000000, sendingAmount: 0 },
+    ];
+    const whitelistTree = createWhitelistTree(whitelists);
+    const whitelistRoot = '0x' + whitelistTree.root().hash.toString('hex');
+    await sut.connect(owner).createEvent(salt, whitelistRoot, c98Token.address, ZERO_ADDRESS);
+
+    await c98Token.connect(owner).mint(sut.address, 3000000);
+    const whilelistProofs = whitelistTree.proofs(2);
+    const proofs = whilelistProofs.map(node => '0x' + node.hash.toString('hex'));
+
+    await hheh.time.increaseTo(currentTimestamp);
+    await expect(sut.connect(account3).redeem(salt, 1, currentTimestamp, account3Address, 1000000, 0, proofs))
+      .to.be.revertedWith('C98Vault: Invalid proof');
+  });
+
+  it('wrong timestamp', async function() {
+    let currentTimestamp = new Date().getTime();
+    const salt = utils.solidityKeccak256(['uint256', 'uint256', 'uint256'], [currentTimestamp, Math.floor(Math.random() * 1000000000), 1005]);
+    let whitelists = [
+      <WhitelistData>{ index: 0, unlockTimestamp: currentTimestamp, recipientAddress: account1Address, receivingAmount: 1000000, sendingAmount: 0 },
+      <WhitelistData>{ index: 1, unlockTimestamp: currentTimestamp, recipientAddress: account2Address, receivingAmount: 1000000, sendingAmount: 0 },
+      <WhitelistData>{ index: 2, unlockTimestamp: currentTimestamp, recipientAddress: account3Address, receivingAmount: 1000000, sendingAmount: 0 },
+    ];
+    const whitelistTree = createWhitelistTree(whitelists);
+    const whitelistRoot = '0x' + whitelistTree.root().hash.toString('hex');
+    await sut.connect(owner).createEvent(salt, whitelistRoot, c98Token.address, ZERO_ADDRESS);
+
+    await c98Token.connect(owner).mint(sut.address, 3000000);
+    const whilelistProofs = whitelistTree.proofs(2);
+    const proofs = whilelistProofs.map(node => '0x' + node.hash.toString('hex'));
+
+    await hheh.time.increaseTo(currentTimestamp);
+    await expect(sut.connect(account3).redeem(salt, 2, currentTimestamp+1, account3Address, 1000000, 0, proofs))
+      .to.be.revertedWith('C98Vault: Invalid proof');
+  });
+
+  it('wrong recipient', async function() {
+    let currentTimestamp = new Date().getTime();
+    const salt = utils.solidityKeccak256(['uint256', 'uint256', 'uint256'], [currentTimestamp, Math.floor(Math.random() * 1000000000), 1006]);
+    let whitelists = [
+      <WhitelistData>{ index: 0, unlockTimestamp: currentTimestamp, recipientAddress: account1Address, receivingAmount: 1000000, sendingAmount: 0 },
+      <WhitelistData>{ index: 1, unlockTimestamp: currentTimestamp, recipientAddress: account2Address, receivingAmount: 1000000, sendingAmount: 0 },
+      <WhitelistData>{ index: 2, unlockTimestamp: currentTimestamp, recipientAddress: account3Address, receivingAmount: 1000000, sendingAmount: 0 },
+    ];
+    const whitelistTree = createWhitelistTree(whitelists);
+    const whitelistRoot = '0x' + whitelistTree.root().hash.toString('hex');
+    await sut.connect(owner).createEvent(salt, whitelistRoot, c98Token.address, ZERO_ADDRESS);
+
+    await c98Token.connect(owner).mint(sut.address, 3000000);
+    const whilelistProofs = whitelistTree.proofs(2);
+    const proofs = whilelistProofs.map(node => '0x' + node.hash.toString('hex'));
+
+    await hheh.time.increaseTo(currentTimestamp);
+    await expect(sut.connect(account3).redeem(salt, 2, currentTimestamp, account2Address, 1000000, 0, proofs))
+      .to.be.revertedWith('C98Vault: Invalid proof');
+  });
+
+  it('wrong amount', async function() {
+    let currentTimestamp = new Date().getTime();
+    const salt = utils.solidityKeccak256(['uint256', 'uint256', 'uint256'], [currentTimestamp, Math.floor(Math.random() * 1000000000), 1007]);
+    let whitelists = [
+      <WhitelistData>{ index: 0, unlockTimestamp: currentTimestamp, recipientAddress: account1Address, receivingAmount: 1000000, sendingAmount: 0 },
+      <WhitelistData>{ index: 1, unlockTimestamp: currentTimestamp, recipientAddress: account2Address, receivingAmount: 1000000, sendingAmount: 0 },
+      <WhitelistData>{ index: 2, unlockTimestamp: currentTimestamp, recipientAddress: account3Address, receivingAmount: 1000000, sendingAmount: 0 },
+    ];
+    const whitelistTree = createWhitelistTree(whitelists);
+    const whitelistRoot = '0x' + whitelistTree.root().hash.toString('hex');
+    await sut.connect(owner).createEvent(salt, whitelistRoot, c98Token.address, ZERO_ADDRESS);
+
+    await c98Token.connect(owner).mint(sut.address, 3000000);
+    const whilelistProofs = whitelistTree.proofs(2);
+    const proofs = whilelistProofs.map(node => '0x' + node.hash.toString('hex'));
+
+    await hheh.time.increaseTo(currentTimestamp);
+    await expect(sut.connect(account3).redeem(salt, 2, currentTimestamp, account3Address, 2000000, 0, proofs))
+      .to.be.revertedWith('C98Vault: Invalid proof');
+  });
+
+  it('wrong proofs', async function() {
+    let currentTimestamp = new Date().getTime();
+    const salt = utils.solidityKeccak256(['uint256', 'uint256', 'uint256'], [currentTimestamp, Math.floor(Math.random() * 1000000000), 1008]);
+    let whitelists = [
+      <WhitelistData>{ index: 0, unlockTimestamp: currentTimestamp, recipientAddress: account1Address, receivingAmount: 1000000, sendingAmount: 0 },
+      <WhitelistData>{ index: 1, unlockTimestamp: currentTimestamp, recipientAddress: account2Address, receivingAmount: 1000000, sendingAmount: 0 },
+      <WhitelistData>{ index: 2, unlockTimestamp: currentTimestamp, recipientAddress: account3Address, receivingAmount: 1000000, sendingAmount: 0 },
+    ];
+    const whitelistTree = createWhitelistTree(whitelists);
+    const whitelistRoot = '0x' + whitelistTree.root().hash.toString('hex');
+    await sut.connect(owner).createEvent(salt, whitelistRoot, c98Token.address, ZERO_ADDRESS);
+
+    await c98Token.connect(owner).mint(sut.address, 3000000);
+    const whilelistProofs = whitelistTree.proofs(1);
+    const proofs = whilelistProofs.map(node => '0x' + node.hash.toString('hex'));
+
+    await hheh.time.increaseTo(currentTimestamp);
+    await expect(sut.connect(account3).redeem(salt, 2, currentTimestamp, account3Address, 1000000, 0, proofs))
+      .to.be.revertedWith('C98Vault: Invalid proof');
+  });
+
+  it('early claim', async function() {
+    let currentTimestamp = new Date().getTime();
+    const salt = utils.solidityKeccak256(['uint256', 'uint256', 'uint256'], [currentTimestamp, Math.floor(Math.random() * 1000000000), 1009]);
+    let whitelists = [
+      <WhitelistData>{ index: 0, unlockTimestamp: currentTimestamp + 100, recipientAddress: account1Address, receivingAmount: 1000000, sendingAmount: 0 },
+      <WhitelistData>{ index: 1, unlockTimestamp: currentTimestamp + 100, recipientAddress: account2Address, receivingAmount: 1000000, sendingAmount: 0 },
+      <WhitelistData>{ index: 2, unlockTimestamp: currentTimestamp + 100, recipientAddress: account3Address, receivingAmount: 1000000, sendingAmount: 0 },
+    ];
+    const whitelistTree = createWhitelistTree(whitelists);
+    const whitelistRoot = '0x' + whitelistTree.root().hash.toString('hex');
+    await sut.connect(owner).createEvent(salt, whitelistRoot, c98Token.address, ZERO_ADDRESS);
+
+    await c98Token.connect(owner).mint(sut.address, 3000000);
+    const whilelistProofs = whitelistTree.proofs(2);
+    const proofs = whilelistProofs.map(node => '0x' + node.hash.toString('hex'));
+
+    await hheh.time.increaseTo(currentTimestamp);
+    await expect(sut.connect(account3).redeem(salt, 2, currentTimestamp + 100, account3Address, 1000000, 0, proofs))
+      .to.be.revertedWith('C98Vault: Schedule locked');
   });
 });
